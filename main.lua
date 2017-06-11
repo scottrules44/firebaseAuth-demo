1ad658976a9962bd06f24bed94eceb0402d20d2f
scottrules44 <scottrules44@gmail.com>
2017-06-11 13:29:34 -0500

    Added Facebook support
    Added Facebook support

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 local firebaseAuth = require "plugin.firebaseAuth"
+local facebook = require( "plugin.facebook.v4" )
 local widget = require("widget")
 local json = require("json")
+
 firebaseAuth.init()
 
 local loadProfilePage
@@ -13,8 +15,10 @@ sceneGroup:insert( profileGroup )
 
 
 
-local emailOfAccount = display.newText( profileGroup, "", display.contentCenterX, display.contentCenterY-100, native.systemFont, 15 )
-local uid = display.newText( profileGroup, "", display.contentCenterX, display.contentCenterY-50, native.systemFont, 12)
+local emailOfAccount = display.newText( profileGroup, "", display.contentCenterX, display.contentCenterY-150, native.systemFont, 15 )
+local uid = display.newText( profileGroup, "", display.contentCenterX, display.contentCenterY-100, native.systemFont, 12)
+local isVerfiyAndAnon = display.newText( profileGroup, "", display.contentCenterX, display.contentCenterY-50, native.systemFont, 12)
+
 profileGroup.alpha = 0
 local signOutButton
 signOutButton = widget.newButton( {
@@ -24,18 +28,39 @@ signOutButton = widget.newButton( {
  labelColor = { default={ 1, 1, 1 }, over={ 0, 0, 0, 0.5 } },
  label = "Sign Out",
  onEvent = function ( e )
- if (e.phase == "ended") then
- firebaseAuth.signOut(function (ev)
-      if(ev.isError) then
-                      native.showAlert( "Could not Sign Out", ev.error ,{"Ok"} )
-      else
-                      signOut(  )
-      end
+  if (e.phase == "ended") then
+    firebaseAuth.signOut(function (ev)
+        if(ev.isError) then
+          native.showAlert( "Could not Sign Out", ev.error ,{"Ok"} )
+        else
+          signOut(  )
+        end
       end)
     end
- end
- } )
+  end
+} )
 profileGroup:insert(signOutButton)
+local sendVerify
+sendVerify = widget.newButton( {
+ x = display.contentCenterX,
+ y = display.contentCenterY+150,
+ id = "sendVerification",
+ labelColor = { default={ 1, 1, 1 }, over={ 0, 0, 0, 0.5 } },
+ label = "Send Verification",
+ onEvent = function ( e )
+  if (e.phase == "ended") then
+    firebaseAuth.sendVerification(function (ev)
+        if(ev.isError) then
+          native.showAlert( "Could not send email", ev.error ,{"Ok"} )
+        else
+          native.showAlert( "Email sent", "" ,{"Ok"} )
+        end
+      end)
+    end
+  end
+} )
+profileGroup:insert(sendVerify)
+
 
 local bg = display.newRect( sceneGroup,display.contentCenterX, display.contentCenterY, display.actualContentWidth, display.actualContentHeight )
 bg:setFillColor( 1,.5,0 )
@@ -94,6 +119,58 @@ createAccount = widget.newButton( {
   end
 } )
 signInGroup:insert( createAccount )
+
+local signInWithFacebook
+signInWithFacebook = widget.newButton( {
+  x = display.contentCenterX,
+  y = display.contentCenterY+160,
+  id = "signInWithFacebook",
+  labelColor = { default={ 0, 0, 1 }, over={ .5, 0, 0, 0.5 } },
+  label = "Sign In with Facebook",
+  onEvent = function ( e )
+    if (e.phase == "ended") then
+        facebook.login(function ( event )
+          if ( "session" == event.type ) then
+            if ( "login" == event.phase ) then
+              local accessToken = facebook.getCurrentAccessToken()
+              firebaseAuth.signInWithFacebook(accessToken, function ( ev )
+                if(ev.isError) then
+                    native.showAlert( "Could not Sign in", ev.error ,{"Ok"} )
+                else
+                    native.showAlert( "Signed in", "" ,{"Ok"} )
+                    loadProfilePage( )
+                end
+              end)
+            end
+          end
+        end)
+    end
+  end
+} )
+signInGroup:insert( signInWithFacebook )
+
+local signInAnonymously
+signInAnonymously = widget.newButton( {
+  x = display.contentCenterX,
+  y = display.contentCenterY+210,
+  id = "signInAnonymously",
+  labelColor = { default={ .5, .5, .5 }, over={ .5, 0, 0, 0.5 } },
+  label = "Sign In Anonymously",
+  onEvent = function ( e )
+    if (e.phase == "ended") then
+      firebaseAuth.signInAnonymously(function ( ev )
+        if(ev.isError) then
+            native.showAlert( "Could not Sign in", ev.error ,{"Ok"} )
+        else
+            native.showAlert( "Signed in", "" ,{"Ok"} )
+            loadProfilePage( )
+        end
+      end)
+    end
+  end
+} )
+signInGroup:insert( signInAnonymously )
+
 signInGroup:toFront( )
 profileGroup:toFront( )
 
@@ -115,11 +192,14 @@ function loadProfilePage( )
   display.remove( email )
   display.remove( password )
   if(firebaseAuth.getEmail()) then
-      emailOfAccount.text="Email:"..firebaseAuth.getEmail()
+    emailOfAccount.text="Email:"..firebaseAuth.getEmail()
+  elseif firebaseAuth.getDisplayName() then
+    emailOfAccount.text="Name:"..firebaseAuth.getDisplayName()
   else
-      emailOfAccount.text="Name:"..firebaseAuth.getDisplayName()
+    emailOfAccount.text="Name: Blank"
   end
   uid.text = "UID:"..firebaseAuth.getUID()
+  isVerfiyAndAnon.text = "Is Account Verfied?:"..tostring(firebaseAuth.isEmailVerified()).."/ Is Anonymous?"..tostring(firebaseAuth.isAnonymous())
 end
 if (firebaseAuth.isSignedIn() and firebaseAuth.isSignedIn() == true) then
   loadProfilePage( )
